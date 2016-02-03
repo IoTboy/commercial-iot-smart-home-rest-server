4171ea5a2222aba9dfaea532b437a3c5209c4039
iotboy <naga.kona@outlook.com>
2016-02-03 01:00:04 +0000

    Rest APIs added

--- server.js
+++ server.js
@@ -22,3 +22,59 @@
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+ app.use(bodyParser.json()); // for parsing application/json
+ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+ app.use(multer()); // for parsing multipart/form-data
+ // For cross-origin requests
+app.use(cors());
+
+// Listen on port 4000
+app.listen(4000);
+
+ function getModes(){
+ 	var mode = JSON.parse(fs.readFileSync("./modes.conf", "utf8"))|| {};
+ 	return JSON.parse(JSON.stringify(mode))|| {};
+ }
+
+ app.get('/home/getmodes', function (req, res) {
+
+ });
+
+ app.post('/home/setmode', function (req, res) {
+
+ });
+
+ app.get('/home/hue/status', function (req, res) {
+
+ });
+
+ app.post('/home/hue/color/livingroom', function (req, res) {
+
+ });
+
+ app.get('/home/hue/color/childrenroom', function (req, res) {
+
+ });
+
+ app.post('/home/cree/status', function (req, res) {
+
+ });
+
+ app.post('/home/cree/brightness', function (req, res) {
+
+ });
+
+ app.post('/home/hue/brightness/livingroom', function (req, res) {
+
+ });
+
+ app.post('/home/hue/brightness/childrenroom', function (req, res) {
+
+ });
+
+ 
